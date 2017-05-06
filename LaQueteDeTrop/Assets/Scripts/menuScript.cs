076a82760761814100545020f2171b5228bb4fbd
Charivarii <lea.boulongne@gmail.com>
2017-05-06 12:10:06 +0200

    Scenes Menu et credits

---
+++ LaQueteDeTrop/Assets/Scripts/menuScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Title screen script
+/// </summary>
+public class menuScript : MonoBehaviour
+{
+    public void StartGame()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("MainScene");
+    }
+
+    public void Credits()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Credits");
+    }
+
+    public void Menu()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Menu");
+    }
+}
+
